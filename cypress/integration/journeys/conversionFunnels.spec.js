c493bb7ba995d58462d4eeb4ed29a9439a8ac73d
Ivo Tijan <ivo.tijan@gmail.com>
2022-02-07 03:02:57 +0100

    add basic conversion funnel test cases

---
+++ cypress/integration/journeys/conversionFunnels.spec.js
@@ -0,0 +1,87 @@
+/// <reference types="cypress" />
+
+context('Pre-Chechout Conversion Funnels', () => {
+
+  // FIXME: was not able to get around redirects so I went with www :/
+  // cy.viewport('iphone-x')
+  // cy.request({
+  //   url: url,
+  //   followRedirect: false, // turn off following redirects
+  // }).then((resp) => {
+  //   // redirect status code is 302
+  //   expect(resp.status).to.eq(302)
+  //   expect(resp.redirectedToUrl).to.eq('https://www.aboutyou.de/')
+  // })
+
+  // clear scenario test data
+  beforeEach(() => {
+    
+  })
+
+  describe('Via direct link', () => {
+    before(() => {
+      const url = 'https://en.aboutyou.de/p/hoermanseder-x-about-you/between-season-jacket-cassia-7059943'
+  
+      cy.visit(url)
+        
+      // accept cookies
+      cy.get('#onetrust-accept-btn-handler').click()
+  
+      // confirm country choice
+      cy.get('[data-testid=countrySwitchCurrentCountry]').click()
+    })
+
+    it('Should be able to select a size', () => {})
+    it('Should see Flyin popup with checkout button', () => {})
+  })
+
+  describe.only('Via page search', () => {
+    before(() => {
+      const url = 'https://www.aboutyou.de/'
+  
+      cy.visit(url)
+        
+      // accept cookies
+      cy.get('#onetrust-accept-btn-handler').click()
+  
+      // confirm country choice
+      cy.get('[data-testid=countrySwitchCurrentCountry]').click()
+  
+      // confirm language switch  
+      cy.get('[data-testid=languageSwitchTargetLanguage]').click()
+    })
+
+    it('Search input field should be visible after clicking search icon', () => {})
+    it('Search dropdown menu should be visible after a search', () => {})
+    it('Product name should be the same as on the search result menu', () => {})
+    it('Should be able to select a size', () => {})
+    it('Should see Flyin popup with checkout button', () => {})
+  })
+
+  describe('Via page category browsing', () => {
+    before(() => {
+      const url = 'https://www.aboutyou.de/'
+  
+      cy.visit(url)
+        
+      // accept cookies
+      cy.get('#onetrust-accept-btn-handler').click()
+  
+      // confirm country choice
+      cy.get('[data-testid=countrySwitchCurrentCountry]').click()
+  
+      // confirm language switch  
+      cy.get('[data-testid=languageSwitchTargetLanguage]').click()
+    })
+
+    // Clothing => Jackets
+    it('Should be able to select a product category from top menu', () => {})
+
+    // Winter Jackets
+    it('Should be able to select a subcategory from side menu', () => {})
+    it('Should be able to select a size', () => {})
+    it('Should see Flyin popup with checkout button', () => {})
+  })
+  
+    
+})
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+// https://github.com/quasarframework/quasar/issues/2233
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false
+  })
